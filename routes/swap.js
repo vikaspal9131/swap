9075a0d2d19886f532d73c9e35e88e954728daa5
vikaspal9131 <vikaspal968562@gmail.com>
2025-07-28 04:57:20 +0530

    fix(redirect):  issue when user send swap request but not get back on main page

--- routes/swap.js
+++ routes/swap.js
@@ -5,24 +5,34 @@ const SwapRequest = require("../models/SwapRequest");
 router.post("/swap-request/:toUserId", async (req, res) => {
   const fromUser = req.session.user?._id;
   const toUser = req.params.toUserId;
-  const { offeredSkill, wantedSkill, message } = req.body;
+  const { offeredSkill, wantedSkill, message } = req.body; 
 
-  if (!fromUser) return res.redirect("/login");
-  if (fromUser === toUser) return res.redirect("back");
+  if (!fromUser) {
+    return res.redirect("/login");
+  }
 
-  const alreadySent = await SwapRequest.findOne({ fromUser, toUser });
-  if (alreadySent) return res.redirect("back");
+  if (fromUser.toString() === toUser.toString()) { 
 
-  await SwapRequest.create({
-    fromUser,
-    toUser,
-    offeredSkill,
-    wantedSkill,
-    message
-  });
+    return res.redirect("/");
+  }
 
-  // Final: Redirect to home
-  res.render("main");
+  try {
+    const alreadySent = await SwapRequest.findOne({ fromUser, toUser });
+    if (alreadySent) {
+      return res.redirect("/");
+    }
+
+    await SwapRequest.create({
+      fromUser,
+      toUser,
+      offeredSkill,
+      wantedSkill,
+      message
+    });
+    res.redirect("/");
+  } catch (error) {
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
